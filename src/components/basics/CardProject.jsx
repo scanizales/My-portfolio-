4046b98fd352ebb674385361c2e252a396cef779
Sofía Canizales <clsofi64@gmail.com>
2025-07-29 17:56:26 -0500

    update

--- src/components/basics/CardProject.jsx
+++ src/components/basics/CardProject.jsx
@@ -5,20 +5,20 @@ const CardProject = ({img, tittle, description, technologies, participation}) =>
     <Card className="py-4 max-w-[750px] flex flex-col gap-3 element border border-transparent hover:border-color-blue p-4 rounded-lg bg-[var(--bg-elements)]">
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start gap-6">
             <h4 className="text-sm  md:text-base font-bold text-center">{tittle}</h4>
-            <small className="text-color-bue text-ms md:text-lg lg:text-lg font-bold">{participation}</small>
-            <small className="text-ms sm:text-base text-[color:var(--text-color)]">{description}</small>
+            <small className="text-color-bue text-ms font-bold">{participation}</small>
+            <small className="text-ms text-[color:var(--text-color)]">{description}</small>
         </CardHeader>
         <CardBody className="overflow-visible py-2">
         <Image
             alt="Card background"
-            className="object-cover rounded-xl"
+            className="object-cover rounded-xl w-20 h-20"
             src={img}
             
         />
         </CardBody>
         <div className="flex flex-wrap gap-3 p-3">
             {technologies.map((technology, index) => (
-                <Code key={index} color="primary" className="text-ms border border-transparent hover:border-color-blue p-2 rounded-lg">{technology}</Code>
+                <Code key={index} color="primary" className="text-xs border border-transparent hover:border-color-blue p-2 rounded-lg">{technology}</Code>
             ))}
         </div>
     </Card>
--- src/components/header/components/ItemNav.jsx
+++ src/components/header/components/ItemNav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const ItemNav = ({ route, img}) => {
   return (
         <li className="transition-transform duration-500 hover:translate-x-3">
-            <Link  to={route}><img src={img} className="w-16 h-14 rounded-full overflow-hidden hover:border-b-2 hover:border-color-blue"></img></Link>
+            <Link  to={route}><img src={img} className="w-10 h-10 rounded-full overflow-hidden hover:border-b-2 hover:border-color-blue"></img></Link>
         </li>
   )
 }
