33445ddc0eb39c16ba617251e9d3247cbbe0fb9b
sofia <clsofi64@gmail.com>
2025-08-17 15:55:37 -0500

    update cv and js

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -1,18 +1,17 @@
-const cursor = document.querySelector('.cursor');
+document.addEventListener("DOMContentLoaded", () => {
+  const cursor = document.querySelector(".cursor");
+  const button = document.querySelector("button");
 
-document.addEventListener('mousemove', (e) => {
-  const mouseX = e.pageX; 
-  const mouseY = e.pageY; 
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = `${e.pageX}px`;
+    cursor.style.top = `${e.pageY}px`;
+  });
 
-  cursor.style.left = `${mouseX}px`;
-  cursor.style.top = `${mouseY}px`;
-});
+  button.addEventListener("mouseenter", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1.5)";
+  });
 
-const button = document.querySelector('button');
-button.addEventListener('mouseenter', () => {
-  cursor.style.transform = "translate(-50%, -50%) scale(1.5)";
-});
-
-button.addEventListener('mouseleave', () => {
-  cursor.style.transform = "translate(-50%, -50%) scale(1)";
+  button.addEventListener("mouseleave", () => {
+    cursor.style.transform = "translate(-50%, -50%) scale(1)";
+  });
 });
